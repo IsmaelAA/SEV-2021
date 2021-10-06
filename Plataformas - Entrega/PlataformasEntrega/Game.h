acbf884b51a9ddf886dddf23dad0597b8af45af2
Ismael Alba Areces <uo270176@uniovi.es>
2021-10-06 12:49:09 +0200

    Plataformas Entrega inicial

---
+++ Plataformas - Entrega/PlataformasEntrega/Game.h
@@ -0,0 +1,57 @@
+#pragma once
+
+// Todas las librerias de SDL y otras utilidades
+#include <iostream>
+#include <string>
+using namespace std;
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <map> 
+
+// Valores generales
+#define WIDTH 480
+#define HEIGHT 320
+
+#include "Layer.h"
+class Layer;
+
+class Game
+{
+public:
+	Game();
+	void scale();
+	bool scaledToMax = false;
+	float scaleLower = 1;
+	void loop();
+	SDL_Window* window; // ventana
+	SDL_Renderer* renderer; // renderizador
+	bool loopActive; // Juego activo
+	Layer* layer;
+	Layer* menuLayer;
+	Layer* gameLayer;
+	TTF_Font* font;
+
+	SDL_Texture* getTexture(string filename);
+	map<string, SDL_Texture*> mapTextures; // map - cache
+
+	int const stateMoving = 1;
+	int const stateJumping = 2;
+	int const stateDying = 3;
+	int const stateDead = 4;
+	int const stateShooting = 5;
+
+	int const orientationRight = 1;
+	int const orientationLeft = 2;
+
+	int currentLevel = 0;
+	int finalLevel = 2;
+
+	int input;
+	int const inputKeyboard = 1;
+	int const inputMouse = 2;
+	int const inputGamePad = 3;
+
+
+
+};
---
+++ Plataformas - Entrega/PlataformasEntrega/GameLayer.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "Layer.h"
+#include "Player.h"
+#include "Background.h"
+
+#include "Enemy.h"
+#include "Projectile.h"
+#include "Text.h"
+#include <fstream> // Leer ficheros
+#include <sstream> // Leer líneas / String
+#include <list>
+
+#include "Audio.h"
+#include "Tile.h"
+#include "Space.h" 
+#include "Pad.h"
+class GameLayer : public Layer
+{
+public:
+	GameLayer(Game* game);
+	void init() override;
+	void processControls() override;
+	void update() override;
+	void draw() override;
+
+	void keysToControls(SDL_Event event);
+	void mouseToControls(SDL_Event event); // USO DE MOUSE
+	void gamePadToControls(SDL_Event event); // USO DE GAMEPAD
+	void loadMap(string name);
+	void loadMapObject(char character, float x, float y);
+	int mapWidth;
+
+	void calculateScroll();
+	float scrollX;
+
+	list<Tile*> tiles;
+
+	Space* space;
+
+	int newEnemyTime = 0;
+
+	Player* player;
+	Background* background;
+	list<Enemy*> enemies;
+	list<Projectile*> projectiles;
+
+	bool controlContinue = false;
+	bool controlShoot = false;
+	int controlMoveY = 0;
+	int controlMoveX = 0;
+
+	Actor* backgroundPoints;
+	Text* textPoints;
+	int points;
+
+	Audio* audioBackground;
+
+
+	Tile* cup; // Elemento de final de nivel
+	// Elementos de interfaz
+	Actor* buttonJump;
+	Actor* buttonShoot;
+
+	Pad* pad;
+	SDL_GameController* gamePad;
+
+	Actor* message;
+	bool pause;
+
+};
+
